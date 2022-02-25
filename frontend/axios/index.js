01a71dcb39aebab34c6730a3af3821541f508960
unknown <lindsay.kelly15@gmail.com>
2022-02-25 11:46:45 -0500

    axioswithauth added

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -1,1 +1,12 @@
+import axios from 'axios'
 // ✨ implement axiosWithAuth
+
+export default function axiosWithAuth() {
+    const token = window.localStorage.getItem('token')
+
+    return axios.create({
+        headers: {
+            Authorization: token
+        }
+    })
+}
